2690b7e9187684b96bbc8a7abc2497195609cd43
Jason R. Jones <slightlynybbled@gmail.com>
2017-08-22 15:55:16 -0400

    Added docstrings, refactored private variables

--- frame.py
+++ frame.py
@@ -7,55 +7,78 @@ logging.basicConfig(level=logging.INFO)
 
 
 class Framer:
+    """
+    Frames and De-frames the data
+    """
     START_OF_FRAME = 0xf7
     END_OF_FRAME = 0x7f
     ESC = 0xf6
     ESC_XOR = 0x20
 
     def __init__(self, port, timeout=0.1, threaded=True):
-        self.port = port
-        self.timeout = timeout
-        self.threaded = threaded
+        self._port = port
+        self._timeout = timeout
+        self._threaded = threaded
 
-        self.raw = []
-        self.messages = []
+        self._raw = []
+        self._messages = []
 
-        if self.threaded:
-            self.runner = threading.Thread(target=self.run, daemon=True)
-            self.runner.start()
+        if self._threaded:
+            self._runner = threading.Thread(target=self.run, daemon=True)
+            self._runner.start()
 
     def tx(self, message):
-        self.port.write([self.START_OF_FRAME])
+        """
+        Transmit a series of bytes
+        :param message: a list of bytes to send
+        :return: None
+        """
+        self._port.write([self.START_OF_FRAME])
 
-        sum1, sum2 = self.fletcher16_checksum(message)
+        sum1, sum2 = self._fletcher16_checksum(message)
         message.append(sum1)
         message.append(sum2)
 
         for b in message:
             if b in [self.START_OF_FRAME, self.END_OF_FRAME, self.ESC]:
-                self.port.write([self.ESC])
-                self.port.write([b ^ self.ESC_XOR])
+                self._port.write([self.ESC])
+                self._port.write([b ^ self.ESC_XOR])
             else:
-                self.port.write([b])
+                self._port.write([b])
 
-        self.port.write([self.END_OF_FRAME])
+        self._port.write([self.END_OF_FRAME])
 
-    def parse_raw_data(self):
-        if self.START_OF_FRAME in self.raw and self.END_OF_FRAME in self.raw:
+    def rx(self):
+        """
+        Receive a series of bytes that have been verified
+        :return: a series of bytes as a tuple or None if empty
+        """
+        try:
+            return tuple(self._messages.pop(0))
+        except IndexError:
+            return None
 
-            while self.raw[0] != self.START_OF_FRAME and len(self.raw) > 0:
-                self.raw.pop(0)
+    def _parse_raw_data(self):
+        """
+        Parses the incoming data and determines if it is valid.  Valid
+        data gets placed into self._messages
+        :return: None
+        """
+        if self.START_OF_FRAME in self._raw and self.END_OF_FRAME in self._raw:
 
-            if self.raw[0] == self.START_OF_FRAME:
-                self.raw.pop(0)
+            while self._raw[0] != self.START_OF_FRAME and len(self._raw) > 0:
+                self._raw.pop(0)
 
-            eof_index = self.raw.index(self.END_OF_FRAME)
-            raw_message = self.raw[:eof_index]
-            self.raw = self.raw[eof_index:]
+            if self._raw[0] == self.START_OF_FRAME:
+                self._raw.pop(0)
+
+            eof_index = self._raw.index(self.END_OF_FRAME)
+            raw_message = self._raw[:eof_index]
+            self._raw = self._raw[eof_index:]
 
             logger.debug('raw message: {}'.format(raw_message))
 
-            message = self.remove_esc_chars(raw_message)
+            message = self._remove_esc_chars(raw_message)
             logger.debug('message with checksum: {}'.format(message))
 
             expected_checksum = (message[-1] << 8) | message[-2]
@@ -64,24 +87,29 @@ class Framer:
             message = message[:-2]
             logger.debug('message: {}'.format(message))
 
-            sum1, sum2 = self.fletcher16_checksum(message)
+            sum1, sum2 = self._fletcher16_checksum(message)
             calculated_checksum = (sum2 << 8) | sum1
 
             if expected_checksum == calculated_checksum:
                 logger.debug('valid message received: {}'.format(message))
-                self.messages.append(message)
+                self._messages.append(message)
             else:
                 logger.warning('invalid message received: {}, discarding'.format(message))
                 logger.debug('expected checksum: {}, calculated checksum: {}'.format(expected_checksum, calculated_checksum))
 
         # remove any extra bytes at the beginning
         try:
-            while self.raw[0] != self.START_OF_FRAME and len(self.raw) > 0:
-                self.raw.pop(0)
+            while self._raw[0] != self.START_OF_FRAME and len(self._raw) > 0:
+                self._raw.pop(0)
         except IndexError:
             pass
 
-    def fletcher16_checksum(self, data):
+    def _fletcher16_checksum(self, data):
+        """
+        Calculates a fletcher16 checksum for the list of bytes
+        :param data: a list of bytes that comprise the message
+        :return:
+        """
         sum1 = 0
         sum2 = 0
 
@@ -95,7 +123,12 @@ class Framer:
 
         return sum1, sum2
 
-    def remove_esc_chars(self, raw_message):
+    def _remove_esc_chars(self, raw_message):
+        """
+        Removes any escape characters from the message
+        :param raw_message: a list of bytes containing the un-processed data
+        :return: a message that has the escaped characters appropriately un-escaped
+        """
         message = []
         escape_next = False
         for c in raw_message:
@@ -111,18 +144,22 @@ class Framer:
         return message
 
     def run(self):
+        """
+        Receives the serial data into the self._raw buffer
+        :return:
+        """
         run_once = True
-        while run_once or self.threaded:
-            waiting = self.port.in_waiting
+        while run_once or self._threaded:
+            waiting = self._port.in_waiting
             if waiting > 0:
-                for c in self.port.read(waiting):
-                    self.raw.append(int(c))
+                for c in self._port.read(waiting):
+                    self._raw.append(int(c))
 
-            self.parse_raw_data()
+            self._parse_raw_data()
             run_once = False
 
-            if self.threaded:
-                time.sleep(self.timeout)
+            if self._threaded:
+                time.sleep(self._timeout)
 
 
 if __name__ == '__main__':
